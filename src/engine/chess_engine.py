27a608514ecbbd110ec4f679e76539fa09ce9565
Mykola Pysarenko <koliapisarenko2006@gmail.com>
2026-02-22 13:05:48 +0200

    feat: implement GameState with initial board matrix

---
+++ src/engine/chess_engine.py
@@ -0,0 +1,12 @@
+class GameState:
+    def __init__(self):
+        self.board = [
+            ["b_rook", "b_knight", "b_bishop", "b_queen", "b_king", "b_bishop", "b_knight", "b_rook"],
+            ["b_pawn", "b_pawn", "b_pawn", "b_pawn", "b_pawn", "b_pawn", "b_pawn", "b_pawn"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["w_pawn", "w_pawn", "w_pawn", "w_pawn", "w_pawn", "w_pawn", "w_pawn", "w_pawn"],
+            ["w_rook", "w_knight", "w_bishop", "w_queen", "w_king", "w_bishop", "w_knight", "w_rook"]
+        ]
