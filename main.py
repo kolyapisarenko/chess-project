c95ccb1ef9008adfc09cecf44a6357ad7bb84048
Mykola Pysarenko <koliapisarenko2006@gmail.com>
2026-02-24 17:20:09 +0200

    feat: add selection and deselect logic to the main event loop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from src import config
 from src.ui.renderer import BoardRenderer
+from src.engine.chess_engine import GameState
 
 def main():
     pygame.init()
@@ -14,6 +15,8 @@ def main():
     player_clicks = []
     selected_square = ()
 
+    gs = GameState()
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -24,15 +27,19 @@ def main():
                     location = pygame.mouse.get_pos()
                     col = (location[0] - config.ORIGIN_X) // config.CELL_SIZE
                     row = (location[1] - config.ORIGIN_Y) // config.CELL_SIZE
-                    if 0 <= col < config.COLS and 0 <= row < config.ROWS:
-                        print(f"Clicked on cell: ({row}, {col})")
-                        selected_square = (row, col)
-                        player_clicks.append(selected_square)
+                    if (row, col) == selected_square:
+                        selected_square = ()
+                        player_clicks = []
+                    else:
+                        if 0 <= col < config.COLS and 0 <= row < config.ROWS:
+                            print(f"Clicked on cell: ({row}, {col})")
+                            selected_square = (row, col)
+                            player_clicks.append(selected_square)
                 else:
                     pass
 
         screen.fill(config.BG_COLOR)
-        renderer.render()
+        renderer.render(gs, selected_square)
         pygame.display.flip()
         clock.tick(config.FPS)
     pygame.quit()
