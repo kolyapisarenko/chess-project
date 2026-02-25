87eea91324e7b5cbecdfb6d69381b89ed8a1ccb2
Mykola Pysarenko <koliapisarenko2006@gmail.com>
2026-02-25 14:11:22 +0200

    feat: integrate move execution and turn validation into main loop

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
 from src import config
 from src.ui.renderer import BoardRenderer
-from src.engine.chess_engine import GameState
+from src.engine.chess_engine import GameState, Move
 
 def main():
     pygame.init()
     pygame.font.init()
+    pygame.mixer.init()
     screen = pygame.display.set_mode((config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
     pygame.display.set_caption(config.TITLE)
     clock = pygame.time.Clock()
@@ -30,11 +31,23 @@ def main():
                     if (row, col) == selected_square:
                         selected_square = ()
                         player_clicks = []
-                    else:
+                    else:        
+                        piece = gs.board[row][col]
+                        color = True if piece[0] == "w" else False
                         if 0 <= col < config.COLS and 0 <= row < config.ROWS:
                             print(f"Clicked on cell: ({row}, {col})")
                             selected_square = (row, col)
                             player_clicks.append(selected_square)
+                        if len(player_clicks) == 1:
+                            if (color != gs.white_to_move) or (piece == "--"):
+                                selected_square = ()
+                                player_clicks = []                            
+                        if len(player_clicks) == 2:
+                            move = Move(player_clicks[0], player_clicks[1], gs.board)
+                            gs.make_move(move)
+                            pygame.mixer.Sound(config.SOUND_PATH + "move-self.wav").play()
+                            selected_square = ()
+                            player_clicks = []
                 else:
                     pass
 
